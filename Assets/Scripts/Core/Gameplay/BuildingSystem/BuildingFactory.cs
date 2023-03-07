8dcc6d6a4b2d417d74368b64c28b6a923b395233
Batuhan Hesapdar <batuhanhesapdar@hotmail.com>
2023-03-07 18:20:11 +0300

    feat: BuildinFactory.cs added

---
+++ Assets/Scripts/Core/Gameplay/BuildingSystem/BuildingFactory.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+namespace StrategyGame.Core.Gameplay.BuildingSystem
+{
+    public class BuildingFactory : MonoBehaviour
+    {
+        public static BuildingFactory Instance { get; private set; }
+        [SerializeField] private GameObject _barrackPrefab;
+        [SerializeField] private GameObject _armoryPrefab;
+        [SerializeField] private GameObject _militaryTowerPrefab;
+        [SerializeField] private GameObject _powerPlantPrefab;
+        private void Awake()
+        {
+            if (Instance != null && Instance != this)
+            {
+                Destroy(this);
+            }
+            else
+            {
+                Instance = this;
+            }
+        }
+        public GameObject GetBuilding(string buildingTypeStr)
+        {
+            GameObject desiredPrefab;
+            switch (buildingTypeStr)
+            {
+                case "Barrack":
+                    desiredPrefab = _barrackPrefab;
+                    break;
+                case "Armory":
+                    desiredPrefab = _armoryPrefab;
+                    break;
+                case "MilitaryTower":
+                    desiredPrefab = _militaryTowerPrefab;
+                    break;
+                case "PowerPlant":
+                    desiredPrefab = _powerPlantPrefab;
+                    break;
+                default:
+                    return null;
+            }
+
+        }
+    }
+}
